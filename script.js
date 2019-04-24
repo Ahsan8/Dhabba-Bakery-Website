23000583a29a9433e1034825629fe1247e2e4464
Ahsan8 <46623076+Ahsan8@users.noreply.github.com>
2019-04-24 13:58:02 +0500

    Add files via upload
    A website for imaginary Dhabba Cafe, where all the information about the available items and services is provided. HTML5, CSS3, Bootstrap 4 and little bit Javascript is used in this website.

---
+++ script.js
@@ -0,0 +1,16 @@
+
+(function ( ) {
+
+document.getElementById('cart-info').addEventListener('click', function( ) {
+
+    const cart=document.getElementById('cart')
+    cart.classList.toggle('show-cart')
+    console.log(cart);
+    
+    
+})
+
+} ( ));
+
+
+// Imediate invoke function for add items
